7cd89894c3b2efae8f8b21a782278439004cf132
Sibin <sibinbk@gmail.com>
2014-12-03 18:51:34 +1100

    Initial Commit

---
+++ TimerFinal/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimerFinal
+//
+//  Created by Sibin Baby on 3/12/2014.
+//  Copyright (c) 2014 Sibin Baby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
